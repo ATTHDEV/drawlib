abda742c40606ecffd367884d34c94e147885150
ATTHDEV <atthawutDev@gmail.com>
2019-01-18 18:52:48 +0700

    fix thread bug

--- drawlib.go
+++ drawlib.go
@@ -287,7 +287,9 @@ func (d *Drawlib) eventLoop() {
 			d.options.Height = size.Y
 			//fmt.Println(d.config.Width, d.config.Height)
 			if d.autoscale {
+				d.mutex.Lock()
 				d.rect = e.Bounds()
+				d.mutex.Unlock()
 			} else {
 				// update canvas position
 				w := d.Canvas.Width()
@@ -297,24 +299,33 @@ func (d *Drawlib) eventLoop() {
 					offsetY := (size.Y - d.Canvas.Height()) / 2
 					offsetW := offsetX + d.Canvas.Width()
 					offsetH := offsetY + d.Canvas.Height()
+					d.mutex.Lock()
 					d.window.Fill(image.Rect(0, 0, offsetX, size.Y), defaultWindowsBackground, draw.Src)
 					d.window.Fill(image.Rect(offsetW, 0, size.X, size.Y), defaultWindowsBackground, draw.Src)
 					d.window.Fill(image.Rect(0, 0, size.X, offsetY), defaultWindowsBackground, draw.Src)
 					d.window.Fill(image.Rect(0, offsetH, size.X, size.Y), defaultWindowsBackground, draw.Src)
 					d.rect = image.Rect(offsetX, offsetY, offsetW, offsetH)
+					d.mutex.Unlock()
+
 				} else if size.X < w || size.Y < h {
 					if size.X < size.Y {
 						offsetY := (size.Y-h)/2 + (w-size.X)/2
 						offsetX := offsetY + size.X
+						d.mutex.Lock()
 						d.window.Fill(image.Rect(0, 0, size.X, offsetY), defaultWindowsBackground, draw.Src)
 						d.window.Fill(image.Rect(0, offsetX, size.X, size.Y), defaultWindowsBackground, draw.Src)
 						d.rect = image.Rect(0, offsetY, size.X, offsetX)
+						d.mutex.Unlock()
+
 					} else {
 						offsetX := (size.X-w)/2 + (h-size.Y)/2
 						offsetY := offsetX + size.Y
+						d.mutex.Lock()
 						d.window.Fill(image.Rect(0, 0, offsetX, size.Y), defaultWindowsBackground, draw.Src)
 						d.window.Fill(image.Rect(offsetY, 0, size.X, size.Y), defaultWindowsBackground, draw.Src)
 						d.rect = image.Rect(offsetX, 0, offsetY, size.Y)
+						d.mutex.Unlock()
+
 					}
 				}
 			}
@@ -330,10 +341,12 @@ func (d *Drawlib) eventLoop() {
 }
 
 func (d *Drawlib) swapbuffer() {
+	d.mutex.Lock()
 	draw.Draw(d.buffer.RGBA(), d.buffer.Bounds(), d.Canvas.im, image.ZP, draw.Src)
 	d.texture.Upload(image.ZP, d.buffer, d.buffer.Bounds())
 	d.window.Scale(d.rect, d.texture, d.texture.Bounds(), draw.Src, nil)
 	d.window.Publish()
+	d.mutex.Unlock()
 }
 
 func (d *Drawlib) CaptureScreen(path string) {
