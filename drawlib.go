ad0e3658e57e27c4328d35eb650565e7d943d3d3
ATTHDEV <atthawutDev@gmail.com>
2019-01-19 19:13:01 +0700

    fix bug

--- drawlib.go
+++ drawlib.go
@@ -391,12 +391,12 @@ func (d *Drawlib) eventLoop() {
 }
 
 func (d *Drawlib) swapbuffer() {
-	d.mutex.Lock()
+	//d.mutex.Lock()
 	draw.Draw(d.buffer.RGBA(), d.buffer.Bounds(), d.Canvas.im, image.ZP, draw.Src)
 	d.texture.Upload(image.ZP, d.buffer, d.buffer.Bounds())
 	d.window.Scale(d.rect, d.texture, d.texture.Bounds(), draw.Src, nil)
 	d.window.Publish()
-	d.mutex.Unlock()
+	//d.mutex.Unlock()
 }
 
 func (d *Drawlib) CaptureScreen(path string) {
