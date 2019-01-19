089bf37baaa1bbf694c5fe84fcc85a7d73ded5b3
ATTHDEV <atthawutDev@gmail.com>
2019-01-19 16:54:45 +0700

    fix bug

--- drawlib.go
+++ drawlib.go
@@ -315,53 +315,53 @@ func (d *Drawlib) eventLoop() {
 		// 	(*d.renderCallback)()
 		// }
 		case size.Event:
-			size := e.Size()
-			d.options.Width = size.X
-			d.options.Height = size.Y
-			if d.autoscale {
-				d.mutex.Lock()
-				d.rect = e.Bounds()
-				d.mutex.Unlock()
-			} else {
-				// update canvas position
-				w := d.Canvas.Width()
-				h := d.Canvas.Height()
-				if size.X >= w && size.Y >= h {
-					offsetX := (size.X - d.Canvas.Width()) / 2
-					offsetY := (size.Y - d.Canvas.Height()) / 2
-					offsetW := offsetX + d.Canvas.Width()
-					offsetH := offsetY + d.Canvas.Height()
-					d.window.Fill(image.Rect(0, 0, offsetX, size.Y), defaultWindowsBackground, draw.Src)
-					d.window.Fill(image.Rect(offsetW, 0, size.X, size.Y), defaultWindowsBackground, draw.Src)
-					d.window.Fill(image.Rect(0, 0, size.X, offsetY), defaultWindowsBackground, draw.Src)
-					d.window.Fill(image.Rect(0, offsetH, size.X, size.Y), defaultWindowsBackground, draw.Src)
-					d.rect = image.Rect(offsetX, offsetY, offsetW, offsetH)
+			// size := e.Size()
+			// d.options.Width = size.X
+			// d.options.Height = size.Y
+			// if d.autoscale {
+			// 	d.mutex.Lock()
+			// 	d.rect = e.Bounds()
+			// 	d.mutex.Unlock()
+			// } else {
+			// 	// update canvas position
+			// 	w := d.Canvas.Width()
+			// 	h := d.Canvas.Height()
+			// 	if size.X >= w && size.Y >= h {
+			// 		offsetX := (size.X - d.Canvas.Width()) / 2
+			// 		offsetY := (size.Y - d.Canvas.Height()) / 2
+			// 		offsetW := offsetX + d.Canvas.Width()
+			// 		offsetH := offsetY + d.Canvas.Height()
+			// 		d.window.Fill(image.Rect(0, 0, offsetX, size.Y), defaultWindowsBackground, draw.Src)
+			// 		d.window.Fill(image.Rect(offsetW, 0, size.X, size.Y), defaultWindowsBackground, draw.Src)
+			// 		d.window.Fill(image.Rect(0, 0, size.X, offsetY), defaultWindowsBackground, draw.Src)
+			// 		d.window.Fill(image.Rect(0, offsetH, size.X, size.Y), defaultWindowsBackground, draw.Src)
+			// 		d.rect = image.Rect(offsetX, offsetY, offsetW, offsetH)
 
-				} else if size.X < w || size.Y < h {
-					if size.X < size.Y {
-						offsetY := (size.Y-h)/2 + (w-size.X)/2
-						offsetX := offsetY + size.X
-						d.mutex.Lock()
-						d.window.Fill(image.Rect(0, 0, size.X, offsetY), defaultWindowsBackground, draw.Src)
-						d.window.Fill(image.Rect(0, offsetX, size.X, size.Y), defaultWindowsBackground, draw.Src)
-						d.rect = image.Rect(0, offsetY, size.X, offsetX)
-						d.mutex.Unlock()
+			// 	} else if size.X < w || size.Y < h {
+			// 		if size.X < size.Y {
+			// 			offsetY := (size.Y-h)/2 + (w-size.X)/2
+			// 			offsetX := offsetY + size.X
+			// 			d.mutex.Lock()
+			// 			d.window.Fill(image.Rect(0, 0, size.X, offsetY), defaultWindowsBackground, draw.Src)
+			// 			d.window.Fill(image.Rect(0, offsetX, size.X, size.Y), defaultWindowsBackground, draw.Src)
+			// 			d.rect = image.Rect(0, offsetY, size.X, offsetX)
+			// 			d.mutex.Unlock()
 
-					} else {
-						offsetX := (size.X-w)/2 + (h-size.Y)/2
-						offsetY := offsetX + size.Y
-						d.mutex.Lock()
-						d.window.Fill(image.Rect(0, 0, offsetX, size.Y), defaultWindowsBackground, draw.Src)
-						d.window.Fill(image.Rect(offsetY, 0, size.X, size.Y), defaultWindowsBackground, draw.Src)
-						d.rect = image.Rect(offsetX, 0, offsetY, size.Y)
-						d.mutex.Unlock()
+			// 		} else {
+			// 			offsetX := (size.X-w)/2 + (h-size.Y)/2
+			// 			offsetY := offsetX + size.Y
+			// 			d.mutex.Lock()
+			// 			d.window.Fill(image.Rect(0, 0, offsetX, size.Y), defaultWindowsBackground, draw.Src)
+			// 			d.window.Fill(image.Rect(offsetY, 0, size.X, size.Y), defaultWindowsBackground, draw.Src)
+			// 			d.rect = image.Rect(offsetX, 0, offsetY, size.Y)
+			// 			d.mutex.Unlock()
 
-					}
-				}
-			}
-			if d.sizeCallback != nil {
-				(*d.sizeCallback)(size.X, size.Y)
-			}
+			// 		}
+			// 	}
+			// }
+			// if d.sizeCallback != nil {
+			// 	(*d.sizeCallback)(size.X, size.Y)
+			// }
 		case updateEvent:
 			d.swapbuffer()
 		case error:
